d64f285606112f1a0e75aa86c779150a408df5fa
gtemplinHelmer <gtemplin@helmerinc.com>
2023-06-21 10:28:36 -0400

    Save

--- instrument_control.py
+++ instrument_control.py
@@ -1,2 +1,49 @@
 # Parent class is template for control of electronic instruments in general
 # BK_Precision class implements that specifically for BK Precision 8601B model
+
+import pyvisa
+import time
+import pandas as pd
+from abc import ABC, abstractmethod
+
+
+class ElectronicTestEquipment(ABC):  # Template class for any electronic load
+
+    @abstractmethod
+    def __init__(self):  # will define all parameters the equipment requires
+        pass
+
+    @abstractmethod
+    def setup(self):  # will fill in the parameters based on user defined inputs
+        pass
+
+    @abstractmethod
+    def direct_input(self, input_value):  # a direct input to change the equipment's functionality in the most basic way
+        pass
+
+    @abstractmethod
+    def read_from_csv(self, csv_file_name):  # what the load will do when reading values from a csv file
+        pass
+
+    @abstractmethod
+    def read_values(self):  # will return the given values for equipment, whatever that may mean
+        pass
+
+
+    # Needs a resource manager object from pyVisa
+    # Needs a load output address to talk to via usb or some other output configuration
+class BK_Precision_8601B(ElectronicTestEquipment):
+
+    def __init__(self, storage_file):
+        self.data_storage_location = storage_file   # likely a CSV file
+
+    # def setup(self):
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 # implementation of BK Precision 8601B Control
+
+import sys
+import signal
+import time
+
+import pyvisa   # Wrapper for VISA
+import pandas   # Read from CSV, save captured data to another CSV
+
+
+def interrupt_handler(signum, frame):
+    print(f'Handling signal {signum} ({signal.Signals(signum).name}).')
+    print("Saving data")
+    time.sleep(2)
+    #   Turn off the load code
+    #   Save the current data code
+    print("Done")
+    sys.exit(0)  # terminate the program
+
+
+def main():
+    print("Starting program")
+    print("Press Ctr1+F2 (Windows) or Ctrl+C (Linux) to terminate the program ")
+
+    resource_manager = pyvisa.ResourceManager()  # object managing all possible test equipment attached
+    print(resource_manager.list_resources())  # shows connections to equipment
+    resource_location = input("Copy and paste the resource to use (not including single quotes): ")  # shows digital 'location' (USB, RS-32, etc)
+
+    #  create equipment object based on what the user inputs (electronic load, function generator, etc)
+
+    time.sleep(60)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, interrupt_handler)
+    main()
